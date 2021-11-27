b4882d95bccfda63136de7ab1e0be6a181becb34
lazaros-batsilas <lbatsilas7@gmail.com>
2021-11-27 09:49:07 +0200

    Initial Commit: Create master

---
+++ SpringCoreCoursera/src/main/java/com/javabrains/demo/SpringCoreCourseraApplication.java
@@ -0,0 +1,13 @@
+package com.javabrains.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreCourseraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCoreCourseraApplication.class, args);
+	}
+
+}
